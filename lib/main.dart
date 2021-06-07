c53d491b8a380e4da06fcc5ab7d16b1fb454929e
Andrey <60741536+EclipseAndrey@users.noreply.github.com>
2021-06-07 11:54:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,65 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+
+/// К предыдущему заданию
+/// const - значение должно быть доступно в момент компиляции
+/// final - тоже const, но значение может быть присвоено позже
+
+
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  int month = 4;
+  switch (month) {
+    case 1: print("Январь");
+      break;
+    case 2:  print( "Февраль");
+      break;
+    case 3: print( "Март");
+      break;
+    case 4: print( "Апрель");
+      break;
+    case 5: print( "Май");
+      break;
+    case 6: print( "Июнь");
+      break;
+    case 7:print( "Июль");
+      break;
+    case 8: print( "Август");
+      break;
+    case 9: print( "Сентябрь");
+      break;
+    case 10: print( "Октябрь");
+      break;
+    case 11: print( "Ноябрь");
+      break;
+    case 12: print( "Декабрь");
+      break;
   }
-}
+ 
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+ 
+  for(int i = 0; i <= 100; i+=2){
+    print(i);
   }
+  
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+
+  var input;
+  int result = 0;
+  String out = "exit";
+  do {
+    print("Введите число");
+     input = stdin.readLineSync();
+     try{
+      result+= int.parse(input);
+      print("Текущая сумма $result");
+      }catch(e){
+        if(input != out){
+        print("Неверное значение");
+        continue;
+        }
+      }
+   
+  } while (input != out);
+  
+  
 }
